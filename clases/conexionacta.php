773f8b60297735c54481291f8564bdc47fb2c145
Josue Lagos <josuelchn@gmail.com>
2021-06-11 19:14:23 -0600

    Update conexionacta.php

--- clases/conexionacta.php
+++ clases/conexionacta.php
@@ -1,5 +1,5 @@
 <?php
-$conn = new mysqli('localhost', 'root','root', 'db-mca');
+$conn = new mysqli('localhost', 'root','root', 'mca');
 mysqli_set_charset($conn, 'utf8');
 if($conn->connection_error){
     echo $error -> $conn->connect_error;
