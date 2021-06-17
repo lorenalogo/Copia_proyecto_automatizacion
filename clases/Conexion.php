a69a74638618f22bee2ffa81c0c74283d4f1d409
Cristian <christiansauceda96@gmail.com>
2021-06-16 21:55:39 -0600

    actualizacion

--- clases/Conexion.php
+++ clases/Conexion.php
@@ -2,10 +2,10 @@
 
 
 
-$servidor= "b6k45kz2tfov8k3gm3lb-mysql.services.clever-cloud.com";
-$usuario= "uaonxvhriuzrnhqp";
-$password = "vAK0fRKcphsgN4apfb5D";
-$base= "b6k45kz2tfov8k3gm3lb";
+$servidor= "localhost";
+$usuario= "root";
+$password = "root";
+$base= "automatizacion";
 
 	$mysqli = new mysqli($servidor, $usuario,$password,$base);
 	$connection = mysqli_connect($servidor, $usuario,$password,$base) or die("Error " . mysqli_error($connection));
@@ -15,7 +15,7 @@ $base= "b6k45kz2tfov8k3gm3lb";
 		die('Error en la conexion' . $mysqli->connect_error);
 		exit();	
 	}
- $connect = new PDO("mysql:host=b6k45kz2tfov8k3gm3lb-mysql.services.clever-cloud.com;dbname=b6k45kz2tfov8k3gm3lb", "uaonxvhriuzrnhqp", "vAK0fRKcphsgN4apfb5D");
+ $connect = new PDO("mysql:host=localhost;dbname=automatizacion", "root", "root");
 
 if (!mysqli_set_charset($mysqli, "utf8")) {
         printf("Error cargando el conjunto de caracteres utf8: %s\n", mysqli_error($mysqli));
