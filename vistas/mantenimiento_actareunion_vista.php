2a2863e65e62a06642d688823742645ef267d472
Cristian <christiansauceda96@gmail.com>
2021-06-18 11:56:49 -0600

    Update mantenimiento_actareunion_vista.php

--- vistas/mantenimiento_actareunion_vista.php
+++ vistas/mantenimiento_actareunion_vista.php
@@ -6,6 +6,29 @@ require_once('../clases/Conexion.php');
 require_once('../clases/funcion_bitacora.php');
 require_once('../clases/funcion_visualizar.php');
 require_once('../clases/funcion_permisos.php');
+
+
+$Id_objeto = 8;
+$visualizacion = permiso_ver($Id_objeto);
+
+
+if ($visualizacion == 0) {
+    // header('location:  ../vistas/menu_roles_vista.php');
+    echo '<script type="text/javascript">
+                swal({
+                    title:"",
+                    text:"Lo sentimos no tiene permiso de visualizar la pantalla",
+                    type: "error",
+                    showConfirmButton: false,
+                    timer: 3000
+                });
+                window.location = "../vistas/menu_roles_vista.php";
+
+        </script>';
+} else {
+    // $respuesta1=$instancia_modelo->listar_select1();
+    bitacora::evento_bitacora($Id_objeto, $_SESSION['id_usuario'], 'Ingreso', 'mantenimiento reunion/acta');
+}
 ?>
 <!DOCTYPE html>
 <html>
@@ -86,7 +109,7 @@ require_once('../clases/funcion_permisos.php');
                                                     <th>Acciones</th>
                                                 </tr>
                                             </thead>
-                                            <tbody> 
+                                            <tbody>
                                                 <?php
                                                 try {
                                                     $sql = "SELECT * FROM tbl_tipo_reunion_acta";
