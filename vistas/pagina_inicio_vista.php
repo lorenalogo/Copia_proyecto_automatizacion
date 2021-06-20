10cb0e6d9281ac4b361d2758ad799ff20c5a2471
Cristian <christiansauceda96@gmail.com>
2021-06-20 17:05:42 -0600

    Update pagina_inicio_vista.php

--- vistas/pagina_inicio_vista.php
+++ vistas/pagina_inicio_vista.php
@@ -7,13 +7,13 @@ if (session_status() === PHP_SESSION_NONE) {
   session_start();
 }
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <meta http-equiv="x-ua-compatible" content="ie=edge">
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Informatica Administrativa</title>
   <!-- SweetAlert2 -->
   <link rel="stylesheet" href="../plugins/sweetalert2-theme-bootstrap-4/bootstrap-4.min.css">
