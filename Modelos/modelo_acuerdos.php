983b780f5365e45fcc36a91fd0dc10c5db3b6d26
Cristian <christiansauceda96@gmail.com>
2021-07-25 21:11:29 -0600

    Update modelo_acuerdos.php

--- Modelos/modelo_acuerdos.php
+++ Modelos/modelo_acuerdos.php
@@ -1,6 +1,9 @@
 <?php
 
 include_once '../clases/Conexion.php';
+require '../plugins/phpmailer/Exception.php';
+require '../plugins/phpmailer/PHPMailer.php';
+require '../plugins/phpmailer/SMTP.php';
 $acta = $_POST['acta'];
 $responsable = $_POST['responsable'];
 $estado = $_POST['estado'];
@@ -11,7 +14,11 @@ $fecha_exp = $_POST['fecha_exp'];
 $fecha_formato = date('Y-m-d', strtotime($fecha_exp));
 $id_registro = $_POST['id_registro'];
 
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
 if ($_POST['acuerdo'] == 'nuevo') {
+    $mail = new PHPMailer(true);
     $estado = 1;
     try {
         $stmt = $mysqli->prepare("INSERT INTO tbl_acuerdos (id_acta, id_participante, id_estado, nombre_acuerdo, descripcion, resolucion, fecha_expiracion) VALUES (?,?,?,?,?,?,?)");
@@ -28,7 +35,62 @@ if ($_POST['acuerdo'] == 'nuevo') {
                 'respuesta' => 'error'
             );
         }
+        //Server settings
+        $mail->SMTPDebug = 0;                      //Enable verbose debug output
+        $mail->isSMTP();                                            //Send using SMTP
+        $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
+        $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+        $mail->Username   = 'informaticaunah21@gmail.com';                     //SMTP username
+        $mail->Password   = 'informaticaunah2021';                               //SMTP password
+        $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
+        $mail->Port       = 465;
+        //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+        //Recipients
+
         $stmt->close();
+        $mail->setFrom('informaticaunah21@gmail.com', 'Jefatura Depto. Informatica Administrativa');
+        $sql = "SELECT
+                t5.num_acta,
+                t4.nombre_reunion,
+                CONCAT_WS(' ', t2.nombres, t2.apellidos) AS nombre,
+                t1.valor AS correo
+                FROM
+                    tbl_contactos t1
+                INNER JOIN tbl_personas t2 ON
+                    t2.id_persona = t1.id_persona
+                INNER JOIN tbl_participantes t3 ON
+                    t3.id_persona = t1.id_persona
+                INNER JOIN tbl_reunion t4 ON
+                    t4.id_reunion = t3.id_reunion
+                INNER JOIN tbl_acta t5 ON t5.id_reunion = t4.id_reunion
+                WHERE
+                    t1.id_tipo_contacto = 4 AND t2.id_persona = $responsable AND t5.id_acta = $acta";
+        $res = $mysqli->query($sql);
+        while ($destino = $res->fetch_assoc()) {
+            $nombrepersona = $destino['nombre'];
+            $email = $destino['correo'];
+            $nombreacta = $destino['num_acta'];
+            $nombrereu = $destino['nombre_reunion'];
+            $mail->addAddress($email,$nombrepersona);
+        }
+        //Content
+        $mail->isHTML(true);                                  //Set email format to HTML
+        $mail->Subject = "Responsable de un nuevo acuerdo";
+        $body .= "Buenas estimado: <strong>$nombrepersona</strong> por medio de la presente se le comunica ";
+        $body .= "que a la reunion <b>$nombrereu</b> a la que asistio se le asigno un acuerdo ";
+        $body .= "con nombre <strong>$nombre_acuerdo</strong> ";
+        $body .= " la cual tiene fecha de vencimiento para el: <strong>$fecha_exp</strong>, con la siguiente descripcion: <br><br>";
+        $body .= "<strong>$descripcion</strong>";
+        $body .= "<br>";
+        $body .= "<br>";
+        $body .= "<h3>Este es un correo automático favor no responder a esta dirección, si quiere contactarse con nosotros por algún motivo escribanos a:</h3>";
+        $body .= "iaunah@unah.edu.hn";
+        $body .= "<br>";
+        $body .= "<br>";
+        $body .= "<h3>Saludos Cordiales, <strong>Depto. IA</strong></h3><br>";
+        $mail->Body = $body;
+        $mail->CharSet = 'UTF-8';
+        $mail->send();
         $mysqli->close();
     } catch (Exception $e) {
         echo "Error: " . $e->getMessage();
