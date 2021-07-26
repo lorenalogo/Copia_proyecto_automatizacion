347a7a895b7cd282c0bd93a3146932e57e26e694
Ramos17K <xkreik@gmail.com>
2021-07-26 16:43:53 -0600

    Update menu_acta_vista.php

--- vistas/menu_acta_vista.php
+++ vistas/menu_acta_vista.php
@@ -3,48 +3,19 @@ require_once('../vistas/pagina_inicio_vista.php');
 require_once('../clases/Conexion.php');
 require_once('../clases/funcion_visualizar.php');
 
-if (permiso_ver('49') == '1') {
+if (permiso_ver('149') == '1') {
 
-    $_SESSION['menu_docentes_vista'] = "...";
+    $_SESSION['actas_pendientes'] = "...";
 } else {
-    $_SESSION['menu_docentes_vista'] = "No tiene permisos para visualizar";
+    $_SESSION['actas_pendientes'] = "No tiene permisos para visualizar";
 }
 
-if (permiso_ver('50') == '1') {
+if (permiso_ver('150') == '1') {
 
-    $_SESSION['registro_docentes_vista'] = "...";
+    $_SESSION['listar_actas'] = "...";
 } else {
-    $_SESSION['registro_docentes_vista'] = "No tiene permisos para visualizar";
+    $_SESSION['listar_Actas'] = "No tiene permisos para visualizar";
 }
-
-if (permiso_ver('51') == '1') {
-
-    $_SESSION['gestion_docentes_vista'] = "...";
-} else {
-    $_SESSION['gestion_docentes_vista'] = "No tiene permisos para visualizar";
-}
-
-if (permiso_ver('52') == '1') {
-
-    $_SESSION['comisiones_actividades_docentes_vista'] = "...";
-} else {
-    $_SESSION['comisiones_actividades_docentes_vista'] = "No tiene permisos para visualizar";
-}
-
-if (permiso_ver('53') == '1') {
-
-    $_SESSION['reporte_carga_academica_docentes_vista'] = "...";
-} else {
-    $_SESSION['reporte_carga_academica_docentes_vista'] = "No tiene permisos para visualizar";
-}
-
-if (permiso_ver('54') == '1') {
-
-    $_SESSION['perfil_docentes_vista'] = "...";
-} else {
-    $_SESSION['perfil_docentes_vista'] = "No tiene permisos para visualizar";
-}
-
 
 ?>
 <!DOCTYPE html>
@@ -69,9 +40,8 @@ if (permiso_ver('54') == '1') {
                         </div><!-- /.col -->
                         <div class="col-sm-6">
                             <ol class="breadcrumb float-sm-right">
-                                <li class="breadcrumb-item"><a href="#">Inicio</a></li>
+                                <li class="breadcrumb-item"><a href="../vistas/pagina_principal_vista.php">Inicio</a></li>
                                 <li class="breadcrumb-item active">Control de actas</li>
-                                <li class="breadcrumb-item active">Actas</li>
                             </ol>
                         </div><!-- /.col -->
                     </div><!-- /.row -->
@@ -89,7 +59,7 @@ if (permiso_ver('54') == '1') {
                             <div class="small-box bg-primary">
                                 <div class="inner">
                                     <h4>Actas Pendientes</h4>
-                                    <p><?php echo $_SESSION['gestion_docentes_vista']; ?></p>
+                                    <p><?php echo $_SESSION['actas_pendientes']; ?></p>
                                 </div>
                                 <div class="icon">
                                 <i class="far fa-clock"></i>
@@ -114,7 +84,7 @@ if (permiso_ver('54') == '1') {
                             <div class="small-box bg-success">
                                 <div class="inner">
                                     <h4>Listar Actas</h4>
-                                    <p><?php echo $_SESSION['reporte_carga_academica_docentes_vista']; ?></p>
+                                    <p><?php echo $_SESSION['listar_actas']; ?></p>
                                 </div>
                                 <div class="icon"><i class="fas fa-file-alt"></i>
                                 </div>
