7ac682ba34ce5c66faa7fa14d452c052118bec77
Christian-sauceda <81939528+Christian-sauceda@users.noreply.github.com>
2021-08-18 10:04:45 -0600

    Update reporte_acta.php

--- Controlador/reporte_acta.php
+++ Controlador/reporte_acta.php
@@ -31,7 +31,7 @@ class myPDF extends FPDF
             $this->SetFont('Arial', '', 12);
             $this->Cell(280, 10, "FECHA: " . $fecha, 0, 0, 'R');
             $this->ln();
-
+            $this->ln();
             $this->SetFont('Arial', 'B', 18);
 
             global $instancia_conexion;
@@ -44,7 +44,7 @@ class myPDF extends FPDF
            
             while ($datos = $stmt->fetch_object()) {
            
-            $this->Cell(300, 10, "REPORTE DE ACTA ".utf8_decode($datos-> num_acta), 0, 0, 'C');
+            $this->Cell(300, 10, "ACTA ".utf8_decode($datos-> num_acta), 0, 0, 'C');
             $this->ln();
             $this->ln();
            
@@ -77,129 +77,39 @@ class myPDF extends FPDF
         $this->cell(0, 10, 'Pagina' . $this->PageNo() . '/{nb}', 0, 0, 'C');
     }
 
-    function headerTable1()
-    {
-        $this->SetX(50);
-        $this->SetFont('Times', 'B', 16);
-        $this->SetFillColor(150,125,255);
-        $this->Cell(60, 7, utf8_decode("Promedio de Asistencia"), 0, 0, 'C');
-        $this->Ln();
-        $this->SetFont('Times', 'B', 12);
-        $this->SetLineWidth(0.3);
-        $this->SetX(50);
-        $this->Cell(70, 7, utf8_decode("N°. Acta"), 1, 0, 'C');
-        $this->Cell(75, 7, utf8_decode("Nombre reunión"), 1, 0, 'C');
-        $this->Cell(40, 7, "Asistencia", 1, 0, 'C');
-        $this->Cell(40, 7, "Inasistencia", 1, 0, 'C');
-        $this->Cell(40, 7, "Excusado", 1, 0, 'C');
-
-        $this->ln();
-        
-        
-    }
-
-    function viewTable1()
-    {
-
-  
-        global $instancia_conexion;
-        $sql="SELECT t1.id_reunion, t1.num_acta, t3.nombre_reunion,
-                ROUND(SUM(t2.id_estado_participante = 1) / COUNT(t2.id_persona) * 100) AS asistio,
-                ROUND(SUM(t2.id_estado_participante = 2) / COUNT(t2.id_persona) * 100) AS inasistencia,
-                ROUND(SUM(t2.id_estado_participante = 3) / COUNT(t2.id_persona) * 100) AS excusa
-                FROM  tbl_acta t1
-                INNER JOIN tbl_participantes t2 ON  t2.id_reunion = t1.id_reunion
-                INNER JOIN tbl_reunion t3 ON t3.id_reunion = t1.id_reunion
-                WHERE t1.id_acta = '$_GET[id];'";
-        $stmt = $instancia_conexion->ejecutarConsulta($sql);
-       
-        while ($total_asistencia = $stmt->fetch_object()) {
-       
-  
-        $this->SetX(50);
-        $this->SetFont('Times', '', 12);
-        $this->Cell(70, 7, utf8_decode($total_asistencia-> num_acta), 1, 0, 'C');
-        $this->Cell(75, 7, utf8_decode($total_asistencia->nombre_reunion), 1, 0, 'C');
-        $this->Cell(40, 7, utf8_decode($total_asistencia->asistio.'%'), 1, 0, 'C');
-        $this->Cell(40, 7, utf8_decode($total_asistencia->inasistencia.'%'), 1, 0, 'C');
-        $this->Cell(40, 7, utf8_decode($total_asistencia->excusa.'%'), 1, 0, 'C');
-        $this->ln();
-        $this->ln();
-        $this->ln();
-        }
-        
-    }
-
-    function headerTable()
-    {
-        $this->SetFont('Times', 'B', 16);
-        $this->SetX(50);
-        $this->Cell(60, 7, 'Lista de Participantes', 0, 0, 'L');
-        $this->ln();
-        
-        $this->SetFont('Times', 'B', 12);
-        $this->SetLineWidth(0.3);
-        $this->SetX(50);
-        $this->Cell(100, 7, "Nombres", 1, 0, 'C');
-        $this->Cell(70, 7, "Estado Asistencia", 1, 0, 'C');
-        $this->Cell(90, 7, "Firma", 1, 0, 'C');
-        $this->ln();
-        
-
-
-       
-    }
-    function viewTable()
-    {
-
     
-      
-        
-        global $instancia_conexion;
-        $sql="SELECT concat_ws(' ', pe.nombres, pe.apellidos)nombres, (ep.estado)'asistencia' 
-                FROM tbl_acta a 
-                INNER JOIN tbl_participantes pa ON pa.id_reunion = a.id_reunion
-                INNER JOIN tbl_personas pe ON pe.id_persona = pa.id_persona
-                INNER JOIN tbl_estado_participante ep ON ep.id_estado = pa.id_estado_participante
-                WHERE a.id_acta= '$_GET[id];'";
-        $stmt = $instancia_conexion->ejecutarConsulta($sql);
-       
-        while ($lista = $stmt->fetch_object()) {
-            $this->SetX(50);
-            $this->SetFont('Times', '', 12);
-            $this->Cell(100, 13, utf8_decode($lista->nombres), 1, 0, 'L');
-            $this->Cell(70, 13, utf8_decode($lista->asistencia), 1, 0, 'C');
-            $this->Cell(90, 13, '', 1, 0, 'C');
-            $this->ln();
-        }
-    }
 
     function informacion()
     {  
               
         global $instancia_conexion;
-        $sql="SELECT r.asunto, r.agenda_propuesta 
-                FROM tbl_reunion r
-                INNER JOIN tbl_acta a ON a.id_reunion = r.id_reunion 
-                WHERE id_acta= '$_GET[id];'";
+        $sql="SELECT
+        r.asunto,
+        a.desarrollo
+    FROM
+        tbl_reunion r
+    INNER JOIN tbl_acta a ON
+        a.id_reunion = r.id_reunion
+    WHERE
+        id_acta = '$_GET[id];'";
         $stmt = $instancia_conexion->ejecutarConsulta($sql);
        
         while ($agenda = $stmt->fetch_object()) {
-            
             $this->Ln();
             $this->Ln();
-        
             $this->SetFont('Times', 'B', 14);
-            $this->Cell(300, 7, utf8_decode('Asunto'), 0, 0, 'L');
+            $this->Cell(300, 7, utf8_decode('Asunto:'), 0, 0, 'L');
             $this->ln();
             $this->SetFont('Times', '', 14);
             $this->MultiCell(350, 7, utf8_decode($agenda->asunto), 0,'L', );
             $this->ln();
+            $this->ln();
             $this->SetFont('Times', 'B', 14);
-            $this->Cell(350, 7, utf8_decode('Agenda Propuesta'), 0, 0, 'L');
+            $this->Cell(350, 7, utf8_decode('Desarrollo de la Reunion:'), 0, 0, 'L');
             $this->ln();
             $this->SetFont('Times', '', 14);
-            $this->MultiCell(300, 7, utf8_decode($agenda->agenda_propuesta), 0, 'J');
+            $this->MultiCell(300, 7, utf8_decode($agenda->desarrollo), 0, 'J');
+            $this->ln();
             $this->ln();
         }
     }
@@ -281,8 +191,104 @@ class myPDF extends FPDF
             $this->Cell(230, 7, utf8_decode($archivos->nombre), 1, 0, 'L');
             $this->Cell(60, 7, utf8_decode($archivos->formato), 1, 0,'L');        
             $this->Ln();
-
+            $this->Ln();
+            $this->Ln();
+            $this->Ln();
+            $this->Ln();
             
+        }
+    }
+    function headerTable1()
+    {
+        $this->SetX(50);
+        $this->SetFont('Times', 'B', 16);
+        $this->SetFillColor(150,125,255);
+        $this->Cell(60, 7, utf8_decode("Promedio de Asistencia"), 0, 0, 'C');
+        $this->Ln();
+        $this->SetFont('Times', 'B', 12);
+        $this->SetLineWidth(0.3);
+        $this->SetX(50);
+        $this->Cell(70, 7, utf8_decode("N°. Acta"), 1, 0, 'C');
+        $this->Cell(75, 7, utf8_decode("Nombre reunión"), 1, 0, 'C');
+        $this->Cell(40, 7, "Asistencia", 1, 0, 'C');
+        $this->Cell(40, 7, "Inasistencia", 1, 0, 'C');
+        $this->Cell(40, 7, "Excusado", 1, 0, 'C');
+
+        $this->ln();
+        
+        
+    }
+    function viewTable1()
+    {
+
+  
+        global $instancia_conexion;
+        $sql="SELECT t1.id_reunion, t1.num_acta, t3.nombre_reunion,
+                ROUND(SUM(t2.id_estado_participante = 1) / COUNT(t2.id_persona) * 100) AS asistio,
+                ROUND(SUM(t2.id_estado_participante = 2) / COUNT(t2.id_persona) * 100) AS inasistencia,
+                ROUND(SUM(t2.id_estado_participante = 3) / COUNT(t2.id_persona) * 100) AS excusa
+                FROM  tbl_acta t1
+                INNER JOIN tbl_participantes t2 ON  t2.id_reunion = t1.id_reunion
+                INNER JOIN tbl_reunion t3 ON t3.id_reunion = t1.id_reunion
+                WHERE t1.id_acta = '$_GET[id];'";
+        $stmt = $instancia_conexion->ejecutarConsulta($sql);
+       
+        while ($total_asistencia = $stmt->fetch_object()) {
+       
+  
+        $this->SetX(50);
+        $this->SetFont('Times', '', 12);
+        $this->Cell(70, 7, utf8_decode($total_asistencia-> num_acta), 1, 0, 'C');
+        $this->Cell(75, 7, utf8_decode($total_asistencia->nombre_reunion), 1, 0, 'C');
+        $this->Cell(40, 7, utf8_decode($total_asistencia->asistio.'%'), 1, 0, 'C');
+        $this->Cell(40, 7, utf8_decode($total_asistencia->inasistencia.'%'), 1, 0, 'C');
+        $this->Cell(40, 7, utf8_decode($total_asistencia->excusa.'%'), 1, 0, 'C');
+        $this->ln();
+        $this->ln();
+        $this->ln();
+        }
+        
+    }
+    
+    
+
+    function headerTable()
+    {
+        $this->SetFont('Times', 'B', 16);
+        $this->SetX(50);
+        $this->Cell(60, 7, 'Lista de Participantes', 0, 0, 'L');
+        $this->ln();
+        
+        $this->SetFont('Times', 'B', 12);
+        $this->SetLineWidth(0.3);
+        $this->SetX(50);
+        $this->Cell(100, 7, "Nombres", 1, 0, 'C');
+        $this->Cell(70, 7, "Estado Asistencia", 1, 0, 'C');
+        $this->Cell(90, 7, "Firma", 1, 0, 'C');
+        $this->ln();
+        
+
+
+       
+    }
+    function viewTable()
+    {
+        global $instancia_conexion;
+        $sql="SELECT concat_ws(' ', pe.nombres, pe.apellidos)nombres, (ep.estado)'asistencia' 
+                FROM tbl_acta a 
+                INNER JOIN tbl_participantes pa ON pa.id_reunion = a.id_reunion
+                INNER JOIN tbl_personas pe ON pe.id_persona = pa.id_persona
+                INNER JOIN tbl_estado_participante ep ON ep.id_estado = pa.id_estado_participante
+                WHERE a.id_acta= '$_GET[id];'";
+        $stmt = $instancia_conexion->ejecutarConsulta($sql);
+       
+        while ($lista = $stmt->fetch_object()) {
+            $this->SetX(50);
+            $this->SetFont('Times', '', 12);
+            $this->Cell(100, 13, utf8_decode($lista->nombres), 1, 0, 'L');
+            $this->Cell(70, 13, utf8_decode($lista->asistencia), 1, 0, 'C');
+            $this->Cell(90, 13, '', 1, 0, 'C');
+            $this->ln();
         }
     }
 }
@@ -294,13 +300,13 @@ $pdf->AddPage('C', 'Legal', 0);
 $pdf->SetMargins(40, 30, 1);
 $pdf->SetAutoPageBreak(true,25);
 $pdf->encabezado();
+$pdf->informacion();
+$pdf->acuerdos();
+$pdf->archivos();
 $pdf->headerTable1();
 $pdf->viewTable1();
 $pdf->headerTable();
 $pdf->viewTable();
-$pdf->informacion();
-$pdf->acuerdos();
-$pdf->archivos();
 
 
 
