c1d1bfeb9715e053f0fb5fbf4452037df3e2bf51
Ramos17K <xkreik@gmail.com>
2021-08-19 17:41:57 -0600

    Update reuniones_pendientes_vista.php

--- vistas/reuniones_pendientes_vista.php
+++ vistas/reuniones_pendientes_vista.php
@@ -8,7 +8,7 @@ require_once('../clases/funcion_visualizar.php');
 require_once('../clases/funcion_permisos.php');
 
 
-$Id_objeto = 147;
+$Id_objeto = 145;
 
 bitacora::evento_bitacora($Id_objeto, $_SESSION['id_usuario'], 'Ingreso', 'A Reuniones Pendientes');
 
