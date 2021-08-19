8b24c0cb29592dc3c77336e898daf460f979aff8
Ramos17K <xkreik@gmail.com>
2021-08-19 17:45:12 -0600

    Update asistencia_persona_vista.php

--- vistas/asistencia_persona_vista.php
+++ vistas/asistencia_persona_vista.php
@@ -8,7 +8,7 @@ require_once('../clases/funcion_bitacora.php');
 require_once('../clases/funcion_visualizar.php');
 require_once('../clases/funcion_permisos.php');
 
-$Id_objeto = 155;
+$Id_objeto = 153;
 $visualizacion = permiso_ver($Id_objeto);
 if ($visualizacion == 0) {
     echo '<script type="text/javascript">
